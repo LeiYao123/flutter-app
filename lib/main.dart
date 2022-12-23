db32b44468abc9a9035f5b7b900672567f58eead
LEIYAO123 <lei@rushable.io>
2022-12-23 20:19:59 +0800

    feat: 优化

--- lib/main.dart
+++ lib/main.dart
@@ -13,12 +13,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Startup Name Generator',
       theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-            backgroundColor: Colors.yellowAccent,
-            foregroundColor: Colors.black
-        )
-      ),
-      home: RandomWords(),
+          appBarTheme: const AppBarTheme(
+              backgroundColor: Colors.yellowAccent,
+              foregroundColor: Colors.black)),
+      home: const RandomWords(),
     );
   }
 }
@@ -39,75 +37,67 @@ class _RandomWordsState extends State<RandomWords> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('startup Name Generator'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.list),
-            onPressed: _pushSaved,
-            tooltip: 'Saved Suggestions'
-          )
-        ],
-      ),
-      body: ListView.builder(
-          padding: const EdgeInsets.all(16.0),
-          itemBuilder: (context, i) {
-            if (i.isOdd) return Divider();
-            final index = i ~/ 2;
-            if (index >= _suggestions.length) {
-              _suggestions.addAll(generateWordPairs().take(10));
-            }
+        appBar: AppBar(
+          title: const Text('startup Name Generator'),
+          actions: [
+            IconButton(
+                icon: const Icon(Icons.list),
+                onPressed: _pushSaved,
+                tooltip: 'Saved Suggestions')
+          ],
+        ),
+        body: ListView.builder(
+            padding: const EdgeInsets.all(16.0),
+            itemBuilder: (context, i) {
+              if (i.isOdd) return const Divider();
+              final index = i ~/ 2;
+              if (index >= _suggestions.length) {
+                _suggestions.addAll(generateWordPairs().take(10));
+              }
 
-            final alreadySaved = _saved.contains(_suggestions[index]);
+              final alreadySaved = _saved.contains(_suggestions[index]);
 
-            return ListTile(
-              title: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-                Text(index.toString() + ' - '),
-                Text(
-                  _suggestions[index].asPascalCase,
-                  style: _biggerFont
-                )
-              ]),
-              trailing: Icon(
-                alreadySaved ? Icons.favorite : Icons.favorite_border,
-                color: alreadySaved ? Colors.red : null,
-                semanticLabel: alreadySaved ? 'Romve from saved' : 'Save',
-              ),
-              onTap: () {
-                setState(() {
-                  if (alreadySaved) {
-                    _saved.remove(_suggestions[index]);
-                  } else {
-                    _saved.add(_suggestions[index]);
-                  }
-                });
-              },
-            );
-          }
-      )
-    );
+              return ListTile(
+                title: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text('$index - '),
+                      Text(_suggestions[index].asPascalCase, style: _biggerFont)
+                    ]),
+                trailing: Icon(
+                  alreadySaved ? Icons.favorite : Icons.favorite_border,
+                  color: alreadySaved ? Colors.red : null,
+                  semanticLabel: alreadySaved ? 'Romve from saved' : 'Save',
+                ),
+                onTap: () {
+                  setState(() {
+                    if (alreadySaved) {
+                      _saved.remove(_suggestions[index]);
+                    } else {
+                      _saved.add(_suggestions[index]);
+                    }
+                  });
+                },
+              );
+            }));
   }
 
   void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (context) {
-          // 此处如何获取 index
-          final tiles = _saved.map((pair) {
-            return ListTile(title: Text(pair.asPascalCase, style: _biggerFont));
-          });
+    Navigator.of(context).push(MaterialPageRoute<void>(builder: (context) {
+      // 此处如何获取 index
+      final tiles = _saved.map((pair) {
+        return ListTile(title: Text(pair.asPascalCase, style: _biggerFont));
+      });
 
-          final divided = tiles.isNotEmpty
-              ? ListTile.divideTiles(context: context, tiles: tiles).toList()
-              : <Widget>[];
+      final divided = tiles.isNotEmpty
+          ? ListTile.divideTiles(context: context, tiles: tiles).toList()
+          : <Widget>[];
 
-          return Scaffold(
-            appBar: AppBar(title: const Text('saved suggestions')),
-            body: ListView(children: divided,)
-          );
-      })
-    );
+      return Scaffold(
+          appBar: AppBar(title: const Text('saved suggestions')),
+          body: ListView(
+            children: divided,
+          ));
+    }));
   }
-
-
 }
